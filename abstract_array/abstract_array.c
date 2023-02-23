6e7f11b8c3dda025d1bfaecb4bf163bcc533a2ee
LukeBeirne <120753966+LukeBeirne@users.noreply.github.com>
2023-02-22 19:42:55 -0500

    Add files via upload

---
+++ abstract_array/abstract_array.c
@@ -0,0 +1,82 @@
+#include "abstract_array.h" 
+#include <string.h>
+
+/*************************************************
+ * Allocation and deallocation functions.
+*************************************************/
+abs_array_t *
+abs_array_create(dims_t dims, size_t elem_size, print_func p_func, free_func f_func)
+{
+	//Creates array struct
+	abs_array_t *array = (abs_array_t *)malloc(sizeof(abs_array_t));
+	
+	//Creates arr with size of x*y*element_size
+	array->arr = (char *)malloc(dims.x*dims.y*elem_size);
+	
+	//Simply copies input values to struct values
+	array->dims = dims;
+	array->type_size = elem_size;
+	array->print_fp = p_func;
+	array->free_fp = f_func;
+	return(array);
+}
+
+void
+abs_array_destroy(abs_array_t *arr)
+{
+	//first frees char *arr in array struct
+	//before freeing the array struct itself
+	free(arr->arr);
+	free(arr);
+    return;
+}
+
+
+
+/*************************************************
+ * Accessor functions.
+*************************************************/
+dims_t
+abs_array_get_dims(abs_array_t *arr)
+{
+	//creates a dims struct to store
+	//x and y before returning it
+	dims_t dims;
+	dims.x = arr->dims.x;
+	dims.y = arr->dims.y;
+	return(dims);
+}
+
+void
+abs_array_get_i_j(abs_array_t *arr, int i, int j, void *ret)
+{
+    return;
+}
+
+
+
+/*************************************************
+ * Mutator functions.
+*************************************************/
+void
+abs_array_set_i_j(abs_array_t *arr, int i, int j, void *val)
+{
+    return;
+}
+
+
+
+/*************************************************
+ * Helper functions.
+*************************************************/
+void
+abs_array_print(abs_array_t *arr)
+{
+    return;
+}
+
+void
+abs_array_apply_func(abs_array_t *arr, apply_func apply, void *priv)
+{
+    return;
+}
---
+++ abstract_array/abstract_array.h
@@ -0,0 +1,68 @@
+/*
+ * Abstract array
+ *
+ * This is the interface to using the abstract array. With an abstract
+ * array, any data type can be stored and retrieved from it. All data
+ * that is stored in the array is copied to and from it. So it makes
+ * deep copies of everything.
+ */
+
+#ifndef ABSTRACT_ARRAY
+#define ABSTRACT_ARRAY
+
+#include <stdlib.h>
+#include <stdio.h>
+
+/*
+ * Function pointer typedefs
+ */
+typedef void (*print_func)(void *);
+typedef void (*free_func)(void *);
+typedef void (*apply_func)(void *, void *);
+
+/*
+ * Dims struct. At the moment we only allow the array to be 2-dimensional.
+ * This struct is used to set and retrieve the dimensions of the array.
+ */
+typedef struct dims
+{
+    int x;
+    int y;
+} dims_t;
+
+typedef struct abs_array_s
+{
+	dims_t dims;		/* Array dimensions */
+	char *arr;		/* Contiguous region data is stored in */
+	size_t type_size;	/* Size of each element in array */
+	print_func print_fp;	/* Print function for the array elements */
+	free_func free_fp;	/* Free function for the array elements */
+} abs_array_t;
+
+/*
+ * Allocation and deallocation functions.
+ */
+abs_array_t *abs_array_create(dims_t dims, size_t elem_size,
+    print_func p_func, free_func f_func);
+void abs_array_destroy(abs_array_t *arr);
+
+/*
+ * Accessor functions.
+ */
+dims_t abs_array_get_dims(abs_array_t *arr);
+void abs_array_get_i_j(abs_array_t *arr, int i, int j, void *ret);
+
+/*
+ * Mutator functions.
+ * NOTE: The array makes copies of all data passed to set_i_j. So it is the
+ * users responsibility to free any val set that was dynamically alloated!
+ */
+void abs_array_set_i_j(abs_array_t *arr, int i, int j, void *val);
+
+/*
+ * Helper functions.
+ */
+void abs_array_print(abs_array_t *arr);
+void abs_array_apply_func(abs_array_t *arr, apply_func apply, void *priv);
+
+#endif /* ABSTRACT_ARRAY */
